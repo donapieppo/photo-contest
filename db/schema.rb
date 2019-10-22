2abda225b976671538038f232d91a274edac5246
Donapieppo <donapieppo@yahoo.it>
2019-10-22 10:21:59 +0200

    shcema

--- db/schema.rb
+++ db/schema.rb
@@ -33,10 +33,21 @@ ActiveRecord::Schema.define(version: 2019_10_02_125639) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "photos", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id", null: false, unsigned: true
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "fk_user_photo"
+  end
+
   create_table "users", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "surname"
     t.string "email", default: "", null: false
+    t.boolean "school"
+    t.string "school_name"
+    t.string "school_address"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -51,4 +62,5 @@ ActiveRecord::Schema.define(version: 2019_10_02_125639) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "photos", "users", name: "fk_user_photo"
 end
