55d086ff74241a4a6f54e80b67a2ce71f7ecbb9d
Donapieppo <donapieppo@yahoo.it>
2019-10-25 13:06:10 +0200

    config.autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module PhotoContest
     config.load_defaults 6.0
     config.i18n.default_locale = :it
     config.hosts << "tester.dm.unibo.it"
+    config.autoload_paths += %W(#{Rails.root}/app/pdfs)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
