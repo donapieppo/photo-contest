18cb0b9b2a09afa0e44e581f0eb057ebd47c5771
Donapieppo <donapieppo@yahoo.it>
2019-11-07 10:08:46 +0100

    jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("@fortawesome/fontawesome-free")
 
 import 'bootstrap';
 
+window.$ = $;
+
 import './css/application.scss';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
