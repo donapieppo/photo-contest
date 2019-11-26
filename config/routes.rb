5d1f041c97a141983c8fbe0a0b56138e91d3224d
Donapieppo <donapieppo@yahoo.it>
2019-11-26 16:09:38 +0100

    release_document

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,19 @@ Rails.application.routes.draw do
   end
   resources :photos
   resources :videos
+  resources :users do
+    resources :photos
+    resources :videos
+  end
 
-  get  'edit_user_info',    to: 'users#edit_user_info', as: :edit_user_info
-  get  'printable_release', to: 'users#release', as: :printable_release
-  post 'update_user_info',  to: 'users#update_user_info', as: :update_user_info
-  post 'delete_id_card',    to: 'users#delete_id_card', as: :delete_id_card
+  get   'edit_user_info',          to: 'users#edit_user_info', as: :edit_user_info
+  get   'printable_release',       to: 'users#release', as: :printable_release
+  post  'update_user_info',        to: 'users#update_user_info', as: :update_user_info
+  post  'delete_id_card',          to: 'users#delete_id_card', as: :delete_id_card
+  get   'edit_release_document',   to: 'users#edit_release_document', as: :edit_release_document
+  post  'submit_release_document', to: 'users#submit_release_document', as: :submit_release_document
+  post  'delete_release_document', to: 'users#delete_release_document', as: :delete_release_document
+  
 
   get 'who_impersonate',    to: 'impersonations#who_impersonate',    as: :who_impersonate
   get 'impersonate/:id',    to: 'impersonations#impersonate',        as: :impersonate
