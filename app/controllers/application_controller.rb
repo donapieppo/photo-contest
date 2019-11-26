7e39fe694b698a3a334e13ce67e2ba52ab528334
Donapieppo <donapieppo@yahoo.it>
2019-11-26 17:58:34 +0100

    log_current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   I18n.locale = :it
 
   protect_from_forgery
-  before_action :authenticate_user!, :user_complete_data
+  before_action :authenticate_user!, :log_current_user, :user_complete_data
 
   def user_complete_data
     if current_user && ! current_user.data_complete? 
@@ -17,4 +17,14 @@ class ApplicationController < ActionController::Base
   def modal_page
     params[:modal] && params[:modal] == 'yyy'
   end
+
+  def log_current_user
+    current_user or return true
+    if current_user != true_user
+      logger.info("#{true_user.email} IMPERSONATING #{current_user.email}")
+    else
+      logger.info("Current user: #{current_user.id} #{current_user.email}")
+    end
+  end
+
 end
