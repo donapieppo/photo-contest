805b5c46055cb0e88e4b6d0981424316295b1eef
Donapieppo <donapieppo@yahoo.it>
2019-12-02 09:02:00 +0100

    mailer

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,9 @@ module PhotoContest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    default_url_options[:host] = 'https://tester.dm.unibo.it'
+    config.action_mailer.smtp_settings = { address: 'mailhost.dm.unibo.it', domain:  'dm.unibo.it', enable_starttls_auto: false }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
