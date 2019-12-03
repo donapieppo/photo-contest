89a3586a6a3c465e0d060d398a06c0793b1b2711
Donapieppo <donapieppo@yahoo.it>
2019-12-03 13:38:50 +0100

    refactor with sponsors in config

--- doc/photo-contest.rb
+++ doc/photo-contest.rb
@@ -3,6 +3,16 @@ JURY   = ['fiorella.s@example.com', 'nicola.a@example.it']
 
 MAX_PHOTOS_FOR_THEME = 2
 
+SPONSOR_ORGANIZERS = { dm: { img: 'dm.png', url: 'http://www.eeee.unibo.it/it' } }
+SPONSOR_SUPPORTERS = {
+                       indam: { img: 'indam.png', url: 'https://www.eeee.it' },
+                       pls: { img: 'pls.png', url: 'http://www.eeee.unibo.it/it' },
+                       ghaia: { img: 'ghaia.png', url: 'https://site.unibo.it/eeee-eu-project/en' },
+                       asterisco: { img: 'asterisco.png', url: 'https://www.eeee.srl' },
+                       ossigeno: { img: 'ossigeno.png', url: 'http://eeee.it' },
+                       zamboni: { img: 'zamboni.png', url: 'https://eeee.eeee.com' }
+                      }
+
 module PhotoContest
   class Application < Rails::Application
     config.i18n.default_locale = :it
