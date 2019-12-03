edbee87b506e885a642953105fbffe679acfc0f9
Donapieppo <donapieppo@yahoo.it>
2019-12-03 13:11:40 +0100

    config example

--- doc/photo-contest.rb
+++ doc/photo-contest.rb
@@ -1,12 +1,12 @@
-ADMINS = ['ddddd@yahoo.it']
-JURY   = ['fffff@gmail.com']
+ADMINS = ['donapieppo@example.com', 'xxx@example.it']
+JURY   = ['fiorella.s@example.com', 'nicola.a@example.it']
 
 MAX_PHOTOS_FOR_THEME = 2
 
 module PhotoContest
   class Application < Rails::Application
     config.i18n.default_locale = :it
-    config.hosts << "tester.unibo.it"
+    config.hosts << "photo-tester.dm.unibo.it"
     config.autoload_paths += %W(#{Rails.root}/app/pdfs)
 
     config.contest_interval = [ Date.new(2019, 12, 01), Date.new(2020, 02, 01) ]
