c9e4f3bb4a821648371317d3e00046342ab49cec
Donapieppo <donapieppo@yahoo.it>
2020-01-07 12:00:46 +0100

    vimeo

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,8 @@ class Video < ApplicationRecord
   def embed_url
     if video_url =~ /https:\/\/www\.youtube\.com\/watch\?v=([0-9a-zA-Z]+)/
       return "https://www.youtube.com/embed/#{$1}"
+    elsif video_url =~ /https:\/\/vimeo.com\/([0-9a-zA-Z]+)/
+      return "https://player.vimeo.com/video/#{$1}"
     end
   end
 end
