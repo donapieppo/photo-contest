60a6597efc1c262522869b87a84053adf43d828d
Donapieppo <donapieppo@yahoo.it>
2020-01-07 12:00:39 +0100

    has_many :videos

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,6 @@
 class Theme < ApplicationRecord
   has_many :photos
+  has_many :videos
 
   scope :for_video, -> { where('themes.video = 1') }
   scope :for_photo, -> { where('themes.video = 0') }
@@ -7,5 +8,14 @@ class Theme < ApplicationRecord
   def to_s
     title
   end
+
+  def for_photo?
+    ! for_videos?
+  end
+
+  def for_video?
+    self.video
+  end
+
 end
 
