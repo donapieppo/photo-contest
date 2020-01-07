3c554effe1b20a50a07baa57291e7f1295e277b9
Donapieppo <donapieppo@yahoo.it>
2020-01-07 12:10:03 +0100

    regexp

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,9 +3,9 @@ class Video < ApplicationRecord
   belongs_to :theme
 
   def embed_url
-    if video_url =~ /https:\/\/www\.youtube\.com\/watch\?v=([0-9a-zA-Z]+)/
+    if video_url =~ %r{https://www\.youtube\.com/watch\?v=([0-9a-zA-Z]+)}
       return "https://www.youtube.com/embed/#{$1}"
-    elsif video_url =~ /https:\/\/vimeo.com\/([0-9a-zA-Z]+)/
+    elsif video_url =~ %r{https://vimeo.com/([0-9a-zA-Z]+)}
       return "https://player.vimeo.com/video/#{$1}"
     end
   end
